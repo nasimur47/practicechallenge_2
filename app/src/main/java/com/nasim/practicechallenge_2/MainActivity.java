4b8ce6193158e5cd3121465059b2228b13fd2153
nasimur47 <mdnasimurrahman11@gmail.com>
2022-09-01 20:03:12 +0600

    nogod apps

---
+++ app/src/main/java/com/nasim/practicechallenge_2/MainActivity.java
@@ -0,0 +1,22 @@
+package com.nasim.practicechallenge_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+
+        getSupportActionBar().hide();
+
+    }
+}
